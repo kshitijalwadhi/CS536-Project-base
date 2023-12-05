429d41f8f7264c62032fd6df9f327ad78b7ff992
kshitijalwadhi <kshitijalwadhi@gmail.com>
2023-12-05 04:48:44 -0500

    add detect rpc

--- client.py
+++ client.py
@@ -1,7 +1,16 @@
 import grpc
 
 import object_detection_pb2_grpc
-from object_detection_pb2 import InitRequest, InitResponse, CloseRequest, CloseResponse
+from object_detection_pb2 import InitRequest, InitResponse, CloseRequest, CloseResponse, DetectRequest, DetectResponse
+
+from utilities.helpers import draw_result, yield_frames_from_video
+from utilities.constants import IMG_SIZE
+
+import time
+
+from imutils.video import FileVideoStream
+import cv2
+import pickle
 
 
 class Client:
@@ -22,7 +31,52 @@ class Client:
         resp: CloseResponse = self.stub.close_connection(req)
         print("Close connection for client: {}".format(resp.client_id))
 
+    def send_video(self):
+        print("Sending video...")
+        time.sleep(1)
+
+        vs = FileVideoStream("data/sample.mp4").start()
+
+        try:
+            sequence_number = 0
+            for img in yield_frames_from_video(vs):
+                sequence_number += 1
+                img = cv2.resize(img, (IMG_SIZE, IMG_SIZE))
+                jpg = cv2.imencode('.jpg', img)[1]
+                frame_data = pickle.dumps(jpg)
+
+                req = DetectRequest(
+                    client_id=self.client_id,
+                    fps=self.client_fps,
+                    sequence_number=sequence_number,
+                    frame_data=frame_data
+                )
+
+                resp: DetectResponse = self.stub.detect(req)
+
+                req_dropped: bool = resp.req_dropped
+
+                if req_dropped:
+                    print("Request dropped")
+                    cv2.imshow("Result", img)
+                else:
+                    result = pickle.loads(resp.bboxes.data)
+                    if result:
+                        display = draw_result(img, result)
+                        cv2.imshow("Result", display)
+                    else:
+                        cv2.imshow("Result", img)
+
+                cv2.waitKey(1)
+
+        except grpc._channel._Rendezvous as err:
+            print("Error: {}".format(err))
+        except KeyboardInterrupt:
+            cv2.destroyAllWindows()
+            vs.stop()
+
 
 if __name__ == '__main__':
     client = Client('localhost:50051', 30)
+    client.send_video()
     client.close_connection()
--- server.py
+++ server.py
@@ -2,15 +2,19 @@ import grpc
 
 import object_detection_pb2_grpc
 
-from object_detection_pb2 import InitRequest, InitResponse, CloseRequest, CloseResponse
+from object_detection_pb2 import InitRequest, InitResponse, CloseRequest, CloseResponse, DetectRequest, DetectResponse
 
 from threading import Lock
 from concurrent import futures
 
-from utilities.constants import MAX_CAMERAS
+from utilities.constants import MAX_CAMERAS, IMG_SIZE
+
+from object_detector import ObjectDetector
 
 import time
 import random
+import pickle
+import cv2
 
 
 class Server(object_detection_pb2_grpc.DetectorServicer):
@@ -36,10 +40,30 @@ class Server(object_detection_pb2_grpc.DetectorServicer):
         print("Client with ID {} disconnected".format(request.client_id))
         return CloseResponse(client_id=request.client_id)
 
+    def detect(self, request: DetectRequest, context):
+        with self.lock:
+            self.connected_clients[request.client_id]["fps"] = request.fps
+            self.connected_clients[request.client_id]["size_each_frame"] = len(request.frame_data)
+
+        frame = pickle.loads(request.frame_data)
+        frame = cv2.imdecode(frame, cv2.IMREAD_COLOR)
+        frame = cv2.resize(frame, (IMG_SIZE, IMG_SIZE))
+
+        bboxes, score = self.detector.detect(frame)
+
+        res = DetectResponse(
+            client_id=request.client_id,
+            sequence_number=request.sequence_number,
+            req_dropped=False,
+            bboxes=bboxes
+        )
+
+        return res
+
 
 if __name__ == '__main__':
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=MAX_CAMERAS))
-    object_detection_pb2_grpc.add_DetectorServicer_to_server(Server(), server)
+    object_detection_pb2_grpc.add_DetectorServicer_to_server(Server(detector=ObjectDetector()), server)
     server.add_insecure_port('[::]:50051')
     server.start()
     print("Server started at port 50051")
