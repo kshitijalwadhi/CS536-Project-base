b46ef0a04f49d92f67e4e7b21ac714030e029482
shreyash2106 <shreyash2106@berkeley.edu>
2023-12-05 18:07:07 -0500

    code to calculate drop probabilities and plot OD scores

--- server.py
+++ server.py
@@ -10,11 +10,16 @@ from concurrent import futures
 from utilities.constants import MAX_CAMERAS, IMG_SIZE, BW
 
 from object_detector import ObjectDetector
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
 
+import numpy as np
 import time
 import random
 import pickle
 import cv2
+import threading
 
 
 class Server(object_detection_pb2_grpc.DetectorServicer):
@@ -26,6 +31,8 @@ class Server(object_detection_pb2_grpc.DetectorServicer):
         self.current_load = 0
         self.prob_dropping = {}  # This is what will control our BW allocation
         self.past_scores = {}
+        self.server_start_time = time.time()
+        self.start_times = {}
 
     def init_client(self, request: InitRequest, context):
         with self.lock:
@@ -39,8 +46,49 @@ class Server(object_detection_pb2_grpc.DetectorServicer):
             }
             self.prob_dropping[client_id] = 0
             self.past_scores[client_id] = []
+            self.start_times[client_id] = time.time() - self.server_start_time
         print("Client with ID {} connected".format(client_id))
         return InitResponse(client_id=client_id)
+    
+    def plot_metrics(self):
+        # Ensure this function is called periodically in the server's main loop or a separate thread.
+        print("here")
+        try:
+            # Create a figure with two subplots
+            plt.figure(figsize=(12, 6))
+
+            # Plotting Object Detection Scores
+            plt.subplot(1, 2, 1)
+            for client_id, scores in self.past_scores.items():
+                if scores:
+                    relative_start_time = self.start_times.get(client_id, 0)
+                    averaged_scores = [np.mean(scores[i:i+10]) for i in range(0, len(scores), 10)]
+                    num_chunks = len(averaged_scores)
+                    time_offsets = [relative_start_time + i for i in range(num_chunks)]
+                    plt.plot(time_offsets, averaged_scores, label=f'Client {client_id}')
+            plt.title('Object Detection Scores')
+            plt.xlabel('Time')
+            plt.ylabel('Score')
+            plt.legend()
+
+            # Plotting Probability of Dropped Packets
+            plt.subplot(1, 2, 2)
+            client_ids = list(self.prob_dropping.keys())
+            probabilities = list(self.prob_dropping.values())
+            plt.bar(client_ids, probabilities, color='blue')
+            plt.title('Probability of Dropped Packets')
+            plt.xlabel('Client ID')
+            plt.ylabel('Probability')
+            plt.xticks(client_ids)
+
+            plt.tight_layout()
+            
+            # Save the plot to a file
+            plt.savefig('./server_metrics.png')
+            print("Plot saved to 'server_metrics.png'")
+            plt.close()
+        except Exception as e:
+            print(f"Error occurred during plotting: {e}")
 
     def close_connection(self, request: CloseRequest, context):
         with self.lock:
@@ -49,15 +97,37 @@ class Server(object_detection_pb2_grpc.DetectorServicer):
                 del self.prob_dropping[request.client_id]
         print("Client with ID {} disconnected".format(request.client_id))
         return CloseResponse(client_id=request.client_id)
+    
+    def calculate_weighted_utilization(self, client_id):
+        # Calculate the average score for the client
+        avg_score = sum(self.past_scores[client_id]) / len(self.past_scores[client_id]) if self.past_scores[client_id] else 0
+
+        normalized_score = avg_score
+
+        # Weighted utilization considers both utilization and object detection score
+        return self.connected_clients[client_id]["utilization"] * (1 + normalized_score)
 
     def update_prob_dropping(self):
         # TODO: This function decides the probability of dropping a request, basically the BW allocation
-        if self.current_load > BW:
-            print("Current load is {}, exceeding BW: {}".format(self.current_load, BW))
-            sorted_clients = sorted(self.connected_clients.items(), key=lambda x: x[1]["utilization"], reverse=True)
-            top_client = sorted_clients[0][0]
-            self.prob_dropping[top_client] = 0.5
-            print("Client {} probability of dropping has been updated to: ".format(top_client), self.prob_dropping[top_client])
+        total_utilization = sum(self.calculate_weighted_utilization(client_id) 
+                                for client_id in self.connected_clients)
+
+        if total_utilization > BW:
+            print("Current load is {}, exceeding BW: {}".format(total_utilization, BW))
+            # sorted_clients = sorted(self.connected_clients.items(), key=lambda x: x[1]["utilization"], reverse=True)
+            # top_client = sorted_clients[0][0]
+            # self.prob_dropping[top_client] = 0.5
+            # print("Client {} probability of dropping has been updated to: ".format(top_client), self.prob_dropping[top_client])
+            weighted_utilization_proportions = {client_id: self.calculate_weighted_utilization(client_id) / total_utilization 
+                                                for client_id in self.connected_clients}
+
+            for client_id in self.connected_clients:
+                inverse = 1 - weighted_utilization_proportions[client_id]
+                self.prob_dropping[client_id] = min(1, inverse)
+
+            # Log updated probabilities
+            for client_id, prob in self.prob_dropping.items():
+                print(f"Client {client_id} probability of dropping updated to: {prob}")
         else:
             print("Current load is {}, within BW: {}".format(self.current_load, BW))
             sorted_prob_dropping = sorted(self.prob_dropping.items(), key=lambda x: x[1], reverse=True)
@@ -96,6 +166,8 @@ class Server(object_detection_pb2_grpc.DetectorServicer):
 
         bboxes, score = self.detector.detect(frame)
 
+        print(f"Client {request.client_id} OD Score: {score}")
+
         self.past_scores[request.client_id].append(score)
 
         res = DetectResponse(
@@ -110,15 +182,31 @@ class Server(object_detection_pb2_grpc.DetectorServicer):
 
         return res
 
+def start_plotting_thread(server_instance, interval=5):
+    def plot():
+        while not stop_plotting_thread.is_set():
+            time.sleep(interval)
+            server_instance.plot_metrics()
+            
+
+    stop_plotting_thread = threading.Event()
+    plotting_thread = threading.Thread(target=plot)
+    plotting_thread.start()
+    return stop_plotting_thread, plotting_thread
 
 if __name__ == '__main__':
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=MAX_CAMERAS))
-    object_detection_pb2_grpc.add_DetectorServicer_to_server(Server(detector=ObjectDetector()), server)
+    od_server = Server(detector=ObjectDetector())
+    object_detection_pb2_grpc.add_DetectorServicer_to_server(od_server, server)
     server.add_insecure_port('[::]:50051')
     server.start()
+    stop_event, plotting_thread = start_plotting_thread(od_server)
     print("Server started at port 50051")
     try:
         while True:
             time.sleep(86400)
     except KeyboardInterrupt:
+        server.stop(0)
+        stop_event.set()  # Signal the plotting thread to stop
+        plotting_thread.join()  # Wait for the plotting thread to finish
         server.stop(0)
