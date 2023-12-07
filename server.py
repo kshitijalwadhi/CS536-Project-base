6b821364cf255b00423c6d9d45ad97fc607b0fa3
kshitijalwadhi <kshitijalwadhi@gmail.com>
2023-12-07 15:59:28 -0500

    trying to add scipy

--- server.py
+++ server.py
@@ -18,6 +18,7 @@ import cv2
 
 import numpy as np
 from pulp import LpMaximize, LpProblem, LpVariable, lpSum, LpStatus, PULP_CBC_CMD, LpMinimize
+from scipy.optimize import linprog
 
 
 class Server(object_detection_pb2_grpc.DetectorServicer):
@@ -40,7 +41,7 @@ class Server(object_detection_pb2_grpc.DetectorServicer):
                 "size_each_frame": 0,
                 "utilization": 0
             }
-            self.prob_dropping[client_id] = 0
+            self.prob_dropping[client_id] = 0.0
             self.past_scores[client_id] = []
         print("Client with ID {} connected".format(client_id))
         return InitResponse(client_id=client_id)
@@ -63,30 +64,40 @@ class Server(object_detection_pb2_grpc.DetectorServicer):
         accuracy = {client_id: np.mean(self.past_scores[client_id][-PAST_SCORE_N:]) if self.past_scores[client_id] else 0
                     for client_id in self.connected_clients}
 
-        print("len prob vars: ", len(prob_vars))
+        print("Accuracy arr: ", accuracy)
 
         bw_arr = [self.connected_clients[client_id]["utilization"] * (1 - prob_vars[client_id]) for client_id in self.connected_clients]
-        # min_score_arr = [accuracy[client_id] * (1 - prob_vars[client_id]) / len(self.connected_clients) for client_id in self.connected_clients]
 
-        print("BW array: ", bw_arr)
-        # print("Min score array: ", min_score_arr)
+        prob += lpSum(prob_vars), "Objective function"
 
-        prob += lpSum(bw_arr) <= BW
-
-        # prob += lpSum(min_score_arr) >= MIN_THRESHOLD_EACH
-
-        for client_id in self.connected_clients:
-            prob += (1-prob_vars[client_id]) * accuracy[client_id] >= MIN_THRESHOLD_EACH
+        prob += lpSum(bw_arr) <= BW, "BW constraint"
 
         results = prob.solve(PULP_CBC_CMD(msg=0))
         print(LpStatus[results])
-        # print(prob_vars)
         if LpStatus[results] == 'Optimal':
             for client_id in self.connected_clients:
-                if prob_vars[client_id].varValue is None:
-                    print("ouaaa")
                 if prob_vars[client_id].varValue is not None:
                     self.prob_dropping[client_id] = prob_vars[client_id].varValue
+
+        for client_id in self.connected_clients:
+            print("Client {} probability: ".format(client_id), self.prob_dropping[client_id])
+
+    def update_prob_dropping_scipy(self):
+        obj = [1.0 for client_id in range(len(self.connected_clients))]
+        sum_util = np.sum([self.connected_clients[client_id]["utilization"] for client_id in self.connected_clients])
+        rhs_ineq = [BW - sum_util]
+        lhs_ineq = [[(-1) * self.connected_clients[client_id]["utilization"] for client_id in self.connected_clients]]
+
+        bnd = [(0.001, 0.9) for _ in range(len(self.connected_clients))]
+
+        print("LHS: ", lhs_ineq)
+        print("RHS: ", rhs_ineq)
+
+        opt = linprog(c=obj, A_ub=lhs_ineq, b_ub=rhs_ineq, bounds=bnd, method="revised simplex")
+
+        if opt.success:
+            for i, client_id in enumerate(self.connected_clients):
+                self.prob_dropping[client_id] = opt.x[i]
 
         for client_id in self.connected_clients:
             print("Client {} probability: ".format(client_id), self.prob_dropping[client_id])
@@ -103,7 +114,7 @@ class Server(object_detection_pb2_grpc.DetectorServicer):
                 self.current_load += self.connected_clients[client_id]["utilization"] * (1-self.prob_dropping[client_id])
 
             try:
-                self.update_prob_dropping()
+                self.update_prob_dropping_scipy()
             except Exception as e:
                 print(e)
 
